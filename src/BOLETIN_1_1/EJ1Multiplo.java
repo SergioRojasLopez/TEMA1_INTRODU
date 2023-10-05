285ba189c5db80aa934f4b0e876f078ecab01083
Sergio Rojas <sergiorojaslopez12@gmail.com>
2023-10-05 14:32:15 +0200

    CLASES DIA -> 05/09/2023 EJERCICIOS Boletin 1.1 -> 1

--- src/BOLETIN_1_1/EJ1Multiplo.java
+++ src/BOLETIN_1_1/EJ1Multiplo.java
@@ -1,4 +1,20 @@
 package BOLETIN_1_1;
 
 public class EJ1Multiplo {
+
+    public static void main(String[] args) {
+
+        for (int i = 1;  i <= 100; i++){
+
+            if (i % 7 == 0){
+
+                System.out.println( i + " es multiplo de 7");
+            } else if (i % 13 == 0) {
+
+                System.out.println(i + " es multiplo de 13");
+
+            }
+
+        }
+    }
 }
